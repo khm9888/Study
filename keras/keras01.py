2a5276f6c32d2542ee678a0a17698c0b470341ec
khm9888 <khm9888@naver.com>
2020-05-12 20:38:36 +0900

    gfg

--- keras/keras01.py
+++ keras/keras01.py
@@ -1,23 +1,24 @@
 import numpy as np
 
-values=[]
-for i in range(1,11):
-    values.append(i)
-x=np.array(values)
-y=np.array(values)
+x=np.array([i for i in range(1,11)])
+y=np.array([i for i in range(1,11)])
+
+# print(x)
+# print(y)
 
 from keras.models import Sequential
 from keras.layers import Dense
 
 model=Sequential()
+
 model.add(Dense(1,input_dim=1,activation='relu'))
 
-model.compile(loss='mean_squared_error',optimizer='adam',metrics=['accuracy'])
+model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
 
 model.fit(x,y,epochs=500,batch_size=1)
 
 loss,acc=model.evaluate(x,y,batch_size=1)
 
+print(f"loss : {loss}")
+print(f"acc : {acc}")
 
-print(f'loss : {loss}')
-print(f'acc : {acc}')
--- keras/keras02.py
+++ keras/keras02.py
@@ -1,46 +1,28 @@
+import numpy as np
+
+x_train=np.array([i for i in range(1,11)])
+y_train=np.array([i for i in range(1,11)])
+x_test=np.array([i for i in range(101,111)])
+y_test=np.array([i for i in range(101,111)])
+
+# print(x)
+# print(y)
+
 from keras.models import Sequential
 from keras.layers import Dense
 
-import numpy as np
-
-small=[]
-big=[]
-
-for i in range(1,11):
-    small.append(i)
-    big.append(i+100)
-    
-print(small)
-print(small)
-print(big)
-print(big)
-
-
-# x_train=np.array([1,2,3,4,5,6,7,8,9,10])
-# y_train=np.array([1,2,3,4,5,6,7,8,9,10])
-
-# x_test=np.array([101,102,103,104,105,106,107,108,109,110])
-# y_test=np.array([101,102,103,104,105,106,107,108,109,110])
-
-x_train=np.array(small)
-y_train=np.array(small)
-
-x_test=np.array(big)
-y_test=np.array(big)
-
 model=Sequential()
 
-model.add(Dense(5,input_dim=1,activation='relu'))
+model.add(Dense(1,input_dim=1,activation='relu'))
 model.add(Dense(3))
-model.add(Dense(1,activation='relu'))
-
+model.add(Dense(1))
 
 model.summary()
 
 model.compile(loss='mse',optimizer='adam',metrics=['accuracy'])
+# model.compile(loss='mse',optimizer='adam',metrics=['mse'])
 
-model.fit(x_train,y_train,epochs=10,validation_data=(x_train,y_train))
-model.fit
+model.fit(x_train,y_train,epochs=100,batch_size=1,validation_data=(x_train,y_train))
 
 loss,acc=model.evaluate(x_test,y_test,batch_size=1)
 
@@ -48,4 +30,4 @@ print(f"loss : {loss}")
 print(f"acc : {acc}")
 
 output=model.predict(x_test)
-print(f'결과물 : {output}')
+print(f"output: \n{output}")

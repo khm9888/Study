afdbdab6b6a9587e07fa4e43cec02fc55dd5e094
khm9888 <khm9888@naver.com>
2020-05-23 13:06:14 +0900

    sfdsf

--- homework/part2.py
+++ homework/part2.py
@@ -105,17 +105,69 @@
 
 # # safe_x = x if x is not None else 0
 
-#p33
+# #p33
 
-even= [x for x in range(5) if x%2==0]
+# even= [x for x in range(5) if x%2==0]
+# print(even)
+
+# even_dic= {x for x in range(5) if x%2==0}
+# print(even_dic)
+
+# pairs = [(x,y) for x in range(2,11) for y in range(1,10)]
+
+# print(len(pairs))
+
+# #p34, assert
+
+# assert 1+1 ==2
+# assert 1+1 ==3, "1none"
+
+# # p35 클래스
+
+# class CounterClicker:
+#     def __init__(self,count):
+#         self.count=count
+        
+#     def __repr__(self):
+#         return f"CounterClicker(count={self.count})"
+    
+#     def click(self, num_times =1):
+#         self.count+=num_times
+    
+#     def read(self):
+#         return self.count
+    
+#     def reset(self):
+#         self.count=0
+
+# c=CounterClicker(3)
+# print(c.count)     
+# assert c.read() ==0
+
+# c.click()
+# c.click()
+# assert c.click().read==2
+
+
+# class NoResetClicker(CountingClicker):
+#     def reset(self):
+#         pass
+
+#p37
+
+def generate_range(n):
+    i=0
+    while i < n:
+        yield  i
+        i += 1
+
+print(type(generate_range(10)))        
+for i in range(10):
+    print(f"i : {i}")
+
+
+print(generate_range(10))
+even = (i for i in generate_range(20) if i % 2 ==0)
+
 print(even)
-
-even_dic= {x for x in range(5) if x%2==0}
-print(even_dic)
-
-pairs = [(x,y) for x in range(2,11) for y in range(1,10)]
-
-print(len(pairs))
-
-
-
+print(type(even))

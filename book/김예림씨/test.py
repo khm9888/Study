166c60f16c49d569d44c06a88002cdf2585c101c
khm9888 <khm9888@naver.com>
2020-06-10 05:34:18 +0900

    asc

---
+++ book/김예림씨/test.py
@@ -0,0 +1,65 @@
+# a=10#정수 #int
+# b="안녕하세요"#str
+# c=True #bool
+# d=10.5 #float, 실수
+
+# a="안녕"#str
+
+
+# b="10"
+
+# an = int(b)
+# print
+# print(type(a))
+
+# # for i in range():
+
+# a=10
+# b=20
+# c=30
+
+# avg=a+b+c
+# avg=avg//3
+
+# print(avg)
+
+# # True False
+
+# bool(0)
+
+# print(a>30)#False
+
+#2번
+#주민등록번호를 읽어와서, 생년월일만 출력하기
+
+# name = "steven"#index
+# name[0:3]#
+# name[0]
+# name[1:2]
+
+# print(name[-3:-1])#ve
+
+# 012345
+# -6-5-4-3-2-1
+# print(Nom[7])#1
+# print(Nom[-7])
+
+# print(123+456)#579, #출력할 때 쓰는 함수
+
+# input()#입력할 때 쓰는 함수
+
+# num = input("주민번호 앞자리 6개를 입력해주세요")#str
+# print(num,"6자리가 당신의 생년월일입니다.")
+
+num1 = int(input("가장 좋아하는 숫자를 입력해주세요"))
+print(num1**2)
+
+#4번 주민번호 앞자리6개를 받아와서 월일만 출력하기
+num=input("주민번호 앞자리 입력! :")#str index (o), int 
+print(num[2:])#910428 -> 0:6
+
+
+#계산기 만들기
+
+#값을 입력받는다.
+# 

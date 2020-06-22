1dbd6d8f05ae9258c5a3fd3ec60a3467c8e927b0
khm9888 <khm9888@naver.com>
2020-06-22 16:02:58 +0900

    ㅀㅎㄹ

--- ml/m28_2_eval_graph_cancer.py
+++ ml/m28_2_eval_graph_cancer.py
@@ -1,12 +1,12 @@
 from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import train_test_split as tts
 import numpy as np
-from xgboost import XGBRegressor
-from sklearn.datasets import load_boston
+from xgboost import XGBClassifier
+from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
-boston = load_boston()
+boston = load_breast_cancer()
 
 
 x= boston.data
@@ -15,11 +15,11 @@ y= boston.target
 x_train, x_test, y_train, y_test = tts(x,y,train_size=0.8,
                                                     random_state=66)
 
-xgb = XGBRegressor(n_estimators=100,learning_rate = 0.1)
+xgb = XGBClassifier(n_estimators=100,learning_rate = 0.1)
 
-xgb.fit(x_train,y_train,verbose=True,eval_metric=["logloss","mse"],eval_)
 
-xgb.fit(x_train,y_train,verbose=True, eval_metric=["rmse","logloss"],eval_set=[(x_train,y_train), 
+
+xgb.fit(x_train,y_train,verbose=True, eval_metric=["logloss","rmse","auc"],eval_set=[(x_train,y_train), 
                                                                                (x_test, y_test)],early_stopping_rounds=100)
 #rmse,mae,logloss,error,auc
 
@@ -57,3 +57,15 @@ ax.legend()
 
 plt.ylabel("rmse")
 plt.show()
+
+fig, ax = plt.subplots()
+
+epochs = len(results["validation_0"]["auc"])
+x_axis = range(epochs)
+ax.plot(x_axis,results["validation_0"]["auc"],label="Train")
+ax.plot(x_axis,results["validation_1"]["auc"],label="Test")
+ax.legend()
+
+plt.ylabel("auc")
+plt.show()
+

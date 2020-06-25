69ef5c5a6894ecc4450c334e5c0bad77919fb241
khm9888 <khm9888@naver.com>
2020-06-26 00:04:39 +0900

    sdsd

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
-
-0.9299836019002435
-model.best_params_
-{'colsample_bylevel': 0.30000000000000004, 'colsample_bytree': 0.9, 
- 'learning_rate': 0.01, 'max_depth': 8, 'n_estimators': 1100}
-'''
-XGBRegressor(base_score=0.5, booster='gbtree', colsample_bylevel=1,
-             colsample_bynode=1, colsample_bytree=1, gamma=0, gpu_id=-1,
-             importance_type='gain', interaction_constraints='',
-             learning_rate=0.300000012, max_delta_step=0, max_depth=6,
-             min_child_weight=1, missing=nan, monotone_constraints='()',
-             n_estimators=100, n_jobs=0, num_parallel_tree=1, random_state=0,
-             reg_alpha=0, reg_lambda=1, scale_pos_weight=1, subsample=1,
-             tree_method='exact', validate_parameters=1, verbosity=None)
-'''
-'''
-model.best_params_
-{'learning_rate': 0.008, 'max_depth': 6, 'n_estimators': 1500}
-------------------------------
-------------------------------
-d:\Study\ml\m24_1_2_boston_select_complete.py
-r2
-0.9306399823167621
-'''
+persons=int(input())#몇명?
+people = []#후보들 
+for i in range(persons):
+    people.append(int(input()))#숫자입력
+    
+dasom = people[0]
+candidate = people[1:]
+candidate = sorted(candidate)
+cnt=0#몇번했는지 확인
+if persons==1:
+    pass
+else:
+    dasom = people[0]
+    candidate = people[1:]
+    candidate.sort()
+    while dasom<=candidate[-1]:#무한루프
+        candidate[-1]-=1
+        dasom+=1
+        candidate = sorted(candidate)
+        cnt+=1      
+        # print(people)
+        # print(cnt,people)
+print(cnt)
